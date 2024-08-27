4a92d8268e1c704831c8e835b61309f3591cf0a7
Khadeejah Asif <142526249+Khadeejahh-Asiff@users.noreply.github.com>
2024-08-27 19:48:11 +0500

    Add files via upload

---
+++ Jquery Tutorials/Employee.cs
@@ -0,0 +1,7 @@
+public class Employee
+{
+    public string firstName { get; set; }
+    public string lastName { get; set; }
+    public string gender { get; set; }
+    public int salary { get; set; }
+}
